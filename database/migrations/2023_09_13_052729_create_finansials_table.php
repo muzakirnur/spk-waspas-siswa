fd7fbd218d5b7f09fd074fdeec33556aff935a19
Muzakir Nur <muzakir.nurr@gmail.com>
2023-09-21 18:01:57 +0700

    Menambahkan Migration

---
+++ database/migrations/2023_09_13_052729_create_finansials_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('finansials', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_ayah');
+            $table->string('nama_ibu');
+            $table->string('status_ayah');
+            $table->string('status_ibu');
+            $table->string('pekerjaan_ayah');
+            $table->string('pekerjaan_ibu');
+            $table->integer('penghasilan_ayah');
+            $table->integer('penghasilan_ibu');
+            $table->integer('jumlah_tanggungan');
+            $table->string('kepemilikan_rumah');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('finansials');
+    }
+};
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,15 +38,5 @@
             {{ __('Don\'t you have an account?') }} <a class="font-medium text-indigo-500 hover:text-indigo-600" href="{{ route('register') }}">{{ __('Sign Up') }}</a>
         </div>
         <!-- Warning -->
-        <div class="mt-5">
-            <div class="bg-amber-100 text-amber-600 px-3 py-2 rounded">
-                <svg class="inline w-3 h-3 shrink-0 fill-current" viewBox="0 0 12 12">
-                    <path d="M10.28 1.28L3.989 7.575 1.695 5.28A1 1 0 00.28 6.695l3 3a1 1 0 001.414 0l7-7A1 1 0 0010.28 1.28z" />
-                </svg>
-                <span class="text-sm">
-                    To support you during the pandemic super pro features are free until March 31st.
-                </span>
-            </div>
-        </div>
     </div>
 </x-authentication-layout>
